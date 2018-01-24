fe8cb74a9c733de23583d45c878e8760153eb3ea
Laya Sadegh <laya.sadegh@chasacademy.se>
2018-01-24 23:26:14 +0100

    Add sortable list and coloumn. Add delete button for cards

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-$(function(){
+$(document).ready(function() {
+    function initSort() {
+        $(".list").sortable({
+            cursor: "move",
+            connectWith: ".list",
+            helper: "clone",
+            placeholder: "sortable-placeholder",
+        });
+
+        $(".column").sortable({
+            cursor: "move",
+            connectWith: ".column",
+            helper: "clone",
+            placeholder: "sortable-placeholder",
+        });
+    } 
+
+    initSort();
+
+    // Remove card on click
+    function initRemoveCard () {
+        $('.card').on('click', '.remove-card' , function(e){
+            $(this).closest('.card').remove()
+        });
+    }
+
+    initRemoveCard();
 
 
-});
+}); 

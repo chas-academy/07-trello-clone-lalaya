6b58c4c24c6ee2dfc178e19d50040a567061ab2c
Laya Sadegh <laya.sadegh@chasacademy.se>
2018-01-26 09:42:14 +0100

    add dialog card

--- app.js
+++ app.js
@@ -8,42 +8,61 @@ $(document).ready(function() {
             placeholder: "sortable-placeholder",
         });
 
-        // Sort list
-        $(".board").sortable({
+        // Sort list - think this works better than drag and drop
+        /*$(".board").sortable({
             cursor: "move",
             connectWith: ".board",
             helper: "clone",
-            placeholder: "sortable-placeholder",
-        
-        });
-    } 
+            placeholder: "sortable-placeholder",            
+        }); */
 
+        $(".list").draggable({
+            snap: ".column"    
+        });
+
+        $(".column").droppable({
+            accept: ".list"
+        });
+    }  
+    
     initSort();
+
 
     function initRemove () {
         // Remove card on click
-        $('.card').on('click', '.remove-card' , function(e){
-            $(this).closest('.card').remove()
+        $('body').on('click', '.card .remove-card' , function(e){
+            $(this).closest('.card').remove();
         });
     
         // Remove list on click
         $('.list').on('click', '.remove-list' , function(e){
-            $(this).closest('.list').remove()
+            $(this).closest('.column').remove();
         });
     }
 
     initRemove();
 
 
+    var $cardContainer;
 
     function addCard(event) {
         event.preventDefault();
+        var name = $('#cardName').val();
+
+        var cardHtml = `
+            <div class="card">
+                `+name+`
+                <button class="remove-card">X</button>
+            </div>
+        `;
+
+        $cardContainer.append(cardHtml);
+        
+
+        dialog.dialog("close");
     }
 
-    var formData = $(event.target).offsetParent().find("form").serializeArray();
-
-
-    dialog = $('.new-card').dialog({
+    dialog = $('#newCardForm').dialog({
         autoOpen: false,
         height: 500,
         width: 500,
@@ -56,10 +75,11 @@ $(document).ready(function() {
         }
 
     });
+ 
+    $('.add-new-card').click(function() {
+        dialog.dialog("open");
+        
+        $cardContainer = $(this).parent().find('.card-container');
 
-    $('.new-card').click(function() {
-    dialog.dialog("open"); 
-
-    });
-
-}); 
+    }); 
+});
